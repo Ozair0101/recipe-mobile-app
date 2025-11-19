a21e8a41de59d9e6578944fe644aeca3a7e72e06
Ozair0101 <ozairjan186@gmail.com>
2025-11-19 11:40:52 +0430

    f

---
+++ app/(tabs)/favorites.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function FavoritesScreen() {
+  return (
+    <View>Favorites Screen</View>
+  )
+}
+
+export default FavoritesScreen;
