bcf76d700290c8471ff9dd07372347d52f593c76
Ozair0101 <ozairjan186@gmail.com>
2025-11-19 11:38:37 +0430

    l

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -4,8 +4,8 @@ import 'react-native-reanimated';
 import { ClerkProvider } from '@clerk/clerk-expo';
 import { useColorScheme } from '@/hooks/use-color-scheme';
 import { tokenCache } from '@clerk/clerk-expo/token-cache';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import { COLORS } from '../constants/colors'
+import React from 'react';
+import SaveScreen from '../components/SafeScreen';
 
 export const unstable_settings = {
   anchor: '(tabs)',
@@ -16,9 +16,9 @@ export default function RootLayout() {
 
   return (
    <ClerkProvider tokenCache={tokenCache} publishableKey={process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!}>
-      <SafeAreaView style={{flex:1, backgroundColor: COLORS.background}}>
+     <SaveScreen>
         <Slot/>
-      </SafeAreaView>
+     </SaveScreen>
     </ClerkProvider>
   );
 }
