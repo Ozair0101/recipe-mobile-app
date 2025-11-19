f1cbb1876c8406fa1ed5b9091a7583992c58315e
Ozair0101 <ozairjan186@gmail.com>
2025-11-19 11:38:42 +0430

    l

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Redirect, Stack, Tabs } from 'expo-router';
 import { useAuth } from '@clerk/clerk-expo';
 import { Ionicons } from '@expo/vector-icons';
+import { COLORS } from '@/constants/colors';
 
 export default function TabLayout() {
   const { isSignedIn } = useAuth();
@@ -11,7 +12,28 @@ export default function TabLayout() {
   }
 
   return (
-    <Tabs>
+    <Tabs screenOptions={{
+      headerShown: false,
+      tabBarActiveTintColor: COLORS.primary,
+      tabBarInactiveTintColor: COLORS.textLight,
+      tabBarStyle: {
+        backgroundColor: COLORS.white,
+        borderTopColor: COLORS.border,
+        borderTopWidth: 0,
+        paddingBottom: 8,
+        paddingTop: 8,
+        height: 80, // this styles all related to tab bar
+      },
+      tabBarLabelStyle: {
+        fontSize: 12,
+        fontWeight: '600',
+      },
+      // headerStyle: {
+      //   backgroundColor: COLORS.background,
+      //   borderBottomColor: COLORS.border,
+      //   borderBottomWidth: 1,
+      // }, this related to header
+    }}>
       <Tabs.Screen
         name='index'
         options={{
